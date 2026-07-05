85703687dcfef5a39641761e7ebee2f07fc28f8c
MANISH PRAJAPATI <KUMHARMANISH565@gmail.com>
2026-07-05 17:45:31 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,54 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'shopzone_db',
+//   process.env.DB_USER || 'root',
+//   process.env.DB_PASS || '',
+//   {
+//     host:    process.env.DB_HOST || 'localhost',
+//     port:    parseInt(process.env.DB_PORT) || 3306,
+//     dialect: 'mysql',
+//     logging: false,
+//     pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+//     define: { timestamps: true, underscored: true }
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'shopzone_db',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASS || '',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host:    process.env.DB_HOST || 'localhost',
-    port:    parseInt(process.env.DB_PORT) || 3306,
-    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
     logging: false,
-    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
-    define: { timestamps: true, underscored: true }
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+
+    define: {
+      timestamps: true,
+      underscored: true
+    }
   }
 );
 
